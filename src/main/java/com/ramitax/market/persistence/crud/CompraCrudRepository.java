0dfa7d7ba3de7d36e2dd25831d414e7a3bd253f5
Ramitax <ramiro.ambrosetti@hotmail.com>
2021-03-28 22:58:33 -0300

    Base project

---
+++ src/main/java/com/ramitax/market/persistence/crud/CompraCrudRepository.java
@@ -0,0 +1,11 @@
+package com.ramitax.market.persistence.crud;
+
+import com.ramitax.market.persistence.entity.Compra;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CompraCrudRepository extends CrudRepository<Compra, Integer> {
+    Optional<List<Compra>> findByIdCliente(String idCliente);
+}
---
+++ src/test/java/com/ramitax/market/RamitaxMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramitax.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamitaxMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
